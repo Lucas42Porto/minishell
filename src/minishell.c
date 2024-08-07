bcec88b395979897298c1cb8c8c1a6af50345159
lumarque <lucas2marques.eu@gmail.com>
2024-08-07 01:45:01 +0100

    header update

--- src/minishell.c
+++ src/minishell.c
@@ -6,16 +6,47 @@
 /*   By: lumarque <lumarque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 16:28:39 by lumarque          #+#    #+#             */
-/*   Updated: 2024/06/25 00:48:19 by lumarque         ###   ########.fr       */
+/*   Updated: 2024/08/07 01:41:11 by lumarque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
+void	get_userline(t_shell *shell, char *prompt)
+{
+	prompt = "[minishell]~> ";
+	shell->user_line = ft_strtrim(readline(prompt), SPACES);
+	if (!shell->user_line)
+		clean_exit(shell, NULL);
+	shell->size_line = ft_strlen(shell->user_line);
+}
+
 int main (void)
 {
-	printf("ennv?  %s\n", getenv("USER"));
-	while(1)
-		;
-		return 0;
+	t_shell	shell;
+
+	init_env_and_path(&shell, &shell.env);
+	while(1) 
+	{
+		get_userline(&shell, NULL);
+
+
+	// 	if (ft_strncmp(shell.user_line, "", shell.size_line) == 0)
+	// 		continue;
+	// 	if (shell.size_line > 0)
+	// 		add_history(shell.user_line);
+	// 	if (!lexer(&shell))
+	// 		continue;
+	// 	//ft_skip_space(&shell.user_line);
+	// 	ft_exec_cmd(&shell, shell.user_line);
+
+		if (*shell.user_line)
+		{
+			add_history(shell.user_line);
+			if (!lexer(&shell))
+				continue;
+			ft_exec_cmd(&shell, shell.user_line);
+		}
+	}
+	return 0;
 }
